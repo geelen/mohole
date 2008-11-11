c883944804dc0307c22828323406298e858d5381
Ben Hoskings <ben@hoskings.net>
2008-11-11 23:38:35 +1000

    Added blank users scaffold.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users do |users|
+    users.resources :scripts
+  end
+
   map.resources :scripts
+
   map.root :controller => 'scripts', :action => 'index'
 end
