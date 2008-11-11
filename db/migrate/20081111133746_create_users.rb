413ed95350bdc4bd880abd04ea4799d882d35643
Ben Hoskings <ben@hoskings.net>
2008-11-11 23:39:25 +1000

    Implemented CreateUsers migration.

--- db/migrate/20081111133746_create_users.rb
+++ db/migrate/20081111133746_create_users.rb
@@ -2,6 +2,8 @@ class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
 
+      t.string :name
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081111123442) do
+ActiveRecord::Schema.define(:version => 20081111133746) do
 
   create_table "scripts", :force => true do |t|
     t.string   "base_uri"
     t.text     "content"
     t.integer  "creator_id"
     t.datetime "deleted_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
