3f236ba9ab26fbeb521a3c3eba1298622c40dcb1
Glen Maddern <glen83@gmail.com>
2008-11-12 00:36:49 +1100

    copy-pasted some sweet awful code

--- lib/script_executor.rb
+++ lib/script_executor.rb
@@ -2,25 +2,34 @@ require 'open-uri'
 require 'hpricot'
 
 class ScriptExecutor
+  LinkHacks = [
+          {:proxy => true, :tags => {:href => [:a], :action => [:form]}},
+                  {:proxy => false, :tags => {:href => [:link], :src => [:img, :script, :iframe]}}
+  ]
+
   def initialize
-    
+
   end
-  
+
   def fetch_uri(uri)
     fetching = "http://#{uri}"
     log "Fetching #{fetching.inspect}"
     Hpricot(open(fetching))
   end
-  
-  def execute(doc, uri, rewrites)
+
+  def execute(doc, uri, rewrites, name, base_uri)
     title = doc.at(:title).inner_text
 
     rewrite(doc, uri, rewrites)
 
-    inject_title(title)
+    inject_title(doc, title)
+
+    hack_links(doc, uri, name, base_uri)
+
+    doc.to_s.gsub(/<!--.*?-->/, '')
   end
-  
-  def inject_title(title)
+
+  def inject_title(doc, title)
     if (title_tag = doc.at(:title))
       title_tag.inner_html = "MoHole! - #{title}"
     elsif (head_tag = doc.at(:head))
@@ -29,12 +38,86 @@ class ScriptExecutor
       body_tags.prepend("<head><title>MoHole! - #{title}</title></head>")
     end
   end
-  
+
   def rewrite(doc, uri, rewrites)
-    
+    rewrites.each { |rule|
+      rule['remove'].each { |t| search(doc, t) { |elems| elems.remove } }
+      rule['prepend'].each { |prep| (doc/prep['at']).prepend(prep['insert'].to_s) }
+      rule['inject'].each { |prep|
+        injections = Hash[*prep.map { |k, v| [k.to_s, v.to_s] }.delete_if { |k, _| k == 'at' }.compact.flatten]
+        self.search(doc, prep['at']) { |elems|
+          elems.set(injections)
+        }
+      }
+    }
   end
 
-  def hack_links
-    
+  def hack_links(doc, uri, name, base_uri)
+    LinkHacks.each { |hack|
+      hack[:tags].each { |attr, tags|
+        tags.each { |tag|
+          (doc/"//#{tag}[@#{attr}]").each { |a_tag|
+            a_tag.raw_attributes[attr.to_s] = hack_link(uri, a_tag.attributes[attr.to_s], name, base_uri, hack[:proxy])
+          }
+        }
+      }
+    }
+  end
+
+  private
+
+  def hack_link(request_uri, url, name, base_uri, proxy = false)
+    case url
+    when /^javascript/, /^mailto/:
+      url
+      # when /\.jpg/, /\.png/:
+      # url
+    else
+      uri = URI.parse(url.strip.gsub(/ /, '%20'))
+      result =
+              if proxy
+                "/#{name}/#{base_uri.scheme}://" +
+                        if uri.relative?
+                          base_uri.host
+                        else
+                          #puts "Warning! Proxying non-base url #{uri}" if uri.host != @base_uri.host
+                          uri.host
+                        end + "#{uri.path}" +
+                        if uri.query
+                          "?#{uri.query}"
+                        else
+                          ""
+                        end
+              else
+                if uri.relative?
+                  "#{base_uri.scheme}://#{base_uri.host}" +
+                          if uri.path =~ /^\//
+                            ""
+                          else
+                            URI.parse(request_uri).path.match(/(.*\/)[^\/]+/)[1]
+                          end
+                else
+                  ""
+                end + "#{uri.to_s}"
+              end
+#      puts "Hacking: #{url.inspect} with proxy: #{proxy} gives: #{result}"
+      result
+    end
+  end
+
+  def search(doc, objs)
+    if objs.is_a? Array then
+      objs
+    else
+      [objs]
+    end.each { |obj|
+      if obj.is_a? Hash
+        str = obj['search']
+        indices = obj['at_indices']
+        str.split('/').inject(doc) { |d, s| d/s.to_s }.values_at(*indices).compact.each { |elem| yield elem.search("*") }
+      else
+        yield obj.split('/').inject(doc) { |d, s| d/s.to_s }
+      end
+    }
   end
 end
