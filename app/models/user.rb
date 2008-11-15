a84f0cfac9f0c82faf6d47a9543af3187710003e
Ben Hoskings <ben@hoskings.net>
2008-11-15 22:51:06 +1000

    First draft of user management.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+
+  public_resource_for :read, :index
+  partitioned_resource_for :write
+
+  def createable_by? account
+    true # Anyone can sign up
+  end
+
 end
