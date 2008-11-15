4e735e0c00e9233284eab15ebc7415749d732d8c
Glen Maddern <glen83@gmail.com>
2008-11-15 14:01:59 +1000

    test time win win!

--- test/functional/scripts_controller_test.rb
+++ test/functional/scripts_controller_test.rb
@@ -1,5 +1,13 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class ScriptsControllerTest < ActionController::TestCase
-  #resourceful
+  context "A script" do
+    setup do
+      @script = Script.find :first
+    end
+    
+    should "have a base_uri" do
+      assert_not_nil @script.base_uri
+    end
+  end
 end
