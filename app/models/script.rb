2a558f290ca776d99ddadefbd30f634d792ecb65
Ben Hoskings <ben@hoskings.net>
2008-11-16 20:31:11 +1000

    Changed user association to .creator.

--- app/models/script.rb
+++ app/models/script.rb
@@ -1,5 +1,5 @@
 class Script < ActiveRecord::Base
-  belongs_to :user, :foreign_key => "creator_id"
+  belongs_to :creator, :class_name => 'User', :foreign_key => "creator_id"
 
   public_resource_for :index, :read
   creator_resource_for :write
