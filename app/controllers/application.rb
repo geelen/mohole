0923bfe13ce6f8e44aed03d236e261e650412c68
Ben Hoskings <ben@hoskings.net>
2008-11-16 15:53:41 +1000

    Added layout and updated stylesheet.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,14 +7,16 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'f6fc755d9dddf07e61e55dabaf41c784'
-  
-  # See ActionController::Base for details 
+
+  # See ActionController::Base for details
   # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
+  # from your application log (in this case, all fields with names like "password").
   # filter_parameter_logging :password
-  
+
   include Hammock
-  
+
+  layout 'application'
+
   before_filter :mock_login
   def mock_login
     @current_account = User.find_by_name ENV["USER"]
