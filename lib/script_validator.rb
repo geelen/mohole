965bf0cb723f1dbc63f53243bfd831c4e158f779
Glen Maddern <glen83@gmail.com>
2008-12-08 19:53:41 +1000

    kwalify is in!

--- lib/script_validator.rb
+++ lib/script_validator.rb
@@ -1,10 +1,39 @@
 require File.join(File.dirname(__FILE__), 'either')
 require 'yaml'
+require 'kwalify'
 
 class ScriptValidator
+  SCHEMA = <<-SCHEMA
+---
+type: map
+mapping:
+  "rewrites":
+    type: seq
+    sequence:
+      - type: map
+        mapping:
+          "remove":
+            type: seq
+            sequence:
+              - type: str
+          "prepend":
+            type: seq
+            sequence:
+              - type: map
+                mapping:
+                  "at":
+                    type: str
+                    required: yes
+                  "insert":
+                    type: str
+                    required: yes
+  SCHEMA
+
   def validate(text)
     parsed = YAML.load(text)
-    p parsed
-    Either.leftIf(parsed.is_a?(Hash), parsed, ["missing 'rewrites' base element!"])
+    schema = YAML.load(SCHEMA)
+    validator = Kwalify::Validator.new(schema)
+    errors = validator.validate(parsed)
+    Either.leftIf(errors.empty?, parsed, errors.map { |e| "[#{e.path}] #{e.message}" })
   end
 end
--- test/unit/script_validator_test.rb
+++ test/unit/script_validator_test.rb
@@ -15,14 +15,86 @@ class ScriptValidatorTest < Test::Unit::TestCase
     should "not validate an empty yaml" do
       either = @script_validator.validate("---")
       assert either.isRight
-      assert_equal ["missing 'rewrites' base element!"], either.right
+      assert_equal ["[/] '---': not a mapping."], either.right
     end
 
-    should "validate a yaml" do
+    should "not validate a sequence" do
+      either = @script_validator.validate("---\n- one\n- two\n- three")
+      assert either.isRight
+      assert_equal ["[/] not a mapping."], either.right
+    end
+
+    should "not validate some other map" do
+      either = @script_validator.validate("---\none:\n  - two\n  - three")
+      assert either.isRight
+      assert_equal ["[/one] key 'one:' is undefined."], either.right
+    end
+
+    should "validate the simplest yaml" do
       either = @script_validator.validate("---\nrewrites: []")
       assert either.isLeft
       expectedResult = {'rewrites' => []}
       assert_equal expectedResult, either.left
+    end
+
+    should "validate a yaml with some removes" do
+      either = @script_validator.validate(<<EOYAML)
+---
+rewrites:
+  - remove:
+    - head
+    - body
+EOYAML
+      assert either.isLeft
+      expectedResult = {"rewrites"=>[{"remove"=>["head", "body"]}]}
+      assert_equal expectedResult, either.left
+    end
+
+    should "not validate a yaml with some bad removals" do
+      either = @script_validator.validate(<<EOYAML)
+---
+rewrites:
+  - remove:
+    - head
+    - 5
+EOYAML
+      assert either.isRight
+      assert_equal ["[/rewrites/0/remove/1] '5': not a string."], either.right
+    end
+
+    should "validate a yaml with some injects" do
+      either = @script_validator.validate(<<EOYAML)
+---
+rewrites:
+  - prepend:
+    - at: body
+      insert: "some_string"
+EOYAML
+      assert either.isLeft
+      expectedResult = {"rewrites"=>[{"prepend"=>[{"at" => "body", "insert" => "some_string"}]}]}
+      assert_equal expectedResult, either.left
+    end
+
+    should "validate a yaml with some injects" do
+      either = @script_validator.validate(<<EOYAML)
+---
+rewrites:
+  - prepend:
+    - at: body
+EOYAML
+      assert either.isRight
+      assert_equal ["[/rewrites/0/prepend/0] key 'insert:' is required."], either.right
+    end
+
+    should "validate a yaml with some injects" do
+      either = @script_validator.validate(<<EOYAML)
+---
+rewrites:
+  - prepend:
+    - insert: "some_text"
+EOYAML
+      assert either.isRight
+      assert_equal ["[/rewrites/0/prepend/0] key 'at:' is required."], either.right
     end
   end
 end
